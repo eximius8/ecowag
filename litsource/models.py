6fe25dcf1fc850f1ba0d6ae308c07dbafd64c988
Mikhail Trunov <mikhail.trunov@gmail.com>
2022-04-10 15:40:49 +0300

    Added search to snippets

--- litsource/models.py
+++ litsource/models.py
@@ -1,10 +1,30 @@
+from curses import panel
+from tabnanny import verbose
 from django.db import models
 from wagtail.snippets.models import register_snippet
+from wagtail.search import index
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 @register_snippet
-class LitSource(models.Model):
+class LitSource(index.Indexed, models.Model):
 
-    name = models.CharField(blank=False, null=False, max_length=200, unique=True)
+    name = models.CharField(
+        verbose_name='Название источника',
+        help_text='Укажите название источника (например Приказ Минприроды N 536)',
+        blank=False, null=False, max_length=200, unique=True)
 
+    pannels = [
+        FieldPanel('name'),
+    ]
 
+    search_fields = [
+        index.SearchField('name', partial_match=True),
+    ]
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = 'Источник литературы'
+        verbose_name_plural = 'Источники литературы'
