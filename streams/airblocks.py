3d12beabb9921ee49d262939755211f9f5cfdb75
Mikhail Trunov <mikhail.trunov@gmail.com>
2022-04-15 00:30:45 +0300

    New data for airblocks

--- streams/airblocks.py
+++ streams/airblocks.py
@@ -2,8 +2,10 @@ from wagtail.core.blocks import StructBlock, \
     FloatBlock, ChoiceBlock
 from wagtail.snippets.blocks import SnippetChooserBlock
 from litsource.models import LitSource
+from .structvalues import ClassStructValue, PDKairStructValue, APIRepresentationMixin
 
-class PDKss(StructBlock):
+
+class PDKss(APIRepresentationMixin, StructBlock):
     value = FloatBlock(
         label='Предельно допустимая концентрация в воздухе средне-суточное зоны (ПДКсс) [мг/м3]',
         required=True, min_value=0)
@@ -11,13 +13,16 @@ class PDKss(StructBlock):
         LitSource,
         label="Источник литературы для значения", 
         required=True)
+    abbr = 'ПДКс.с. (мг/м3)'
+    long_name = 'Предельно допустимая концентрация вещества среднесуточная в атмосферном воздухе населенных мест' 
 
     class Meta:
         icon = 'image'
         label = 'ПДК в воздухе средне-суточное'
+        value_class = PDKairStructValue
 
 
-class PDKmr(StructBlock):
+class PDKmr(APIRepresentationMixin, StructBlock):
     value = FloatBlock(
         label='Предельно допустимая концентрация в воздухе максимальная разовая (ПДКмр) [мг/м3]',
         required=True, min_value=0)
@@ -25,13 +30,16 @@ class PDKmr(StructBlock):
         LitSource,
         label="Источник литературы для значения", 
         required=True)
+    abbr = 'ПДКм.р. (мг/м3)'
+    long_name = 'Предельно допустимая концентрация вещества максимально разовая в атмосферном воздухе населенных мест'
 
     class Meta:
         icon = 'image'
         label = 'ПДК в воздухе максимальная разовая'
+        value_class = PDKairStructValue
 
 
-class PDKrz(StructBlock):
+class PDKrz(APIRepresentationMixin, StructBlock):
     value = FloatBlock(
         label='Предельно допустимая концентрация в воздухе рабочей зоны (ПДКрз) [мг/м3]',
         required=True, min_value=0)
@@ -39,13 +47,15 @@ class PDKrz(StructBlock):
         LitSource,
         label="Источник литературы для значения", 
         required=True)
+    abbr = 'ПДКр.з. (мг/м3)'
+    long_name = 'Предельно допустимая концентрация вещества в атмосферном воздухе рабочей зоны'
 
     class Meta:
         icon = 'image'
         label = 'ПДК в воздухе рабочей зоны'
 
 
-class OBUVair(StructBlock):
+class OBUVair(APIRepresentationMixin, StructBlock):
     value = FloatBlock(
         label='Ориентировочно безопасные уровни воздействия в воздухе [мг/м3]',
         required=True, min_value=0)
@@ -53,12 +63,15 @@ class OBUVair(StructBlock):
         LitSource,
         label="Источник литературы для значения", 
         required=True)
+    abbr = 'ОБУВ (мг/м3)'
+    long_name = 'Ориентировочный безопасный уровень воздействия в атмосферном воздухе населенных мест'
 
     class Meta:
         icon = 'image'
         label = 'ОБУВ в воздухе'
+        value_class = PDKairStructValue
 
-class SafetyClassAir(StructBlock):
+class SafetyClassAir(APIRepresentationMixin, StructBlock):
     value = ChoiceBlock(
         label='Класс опасности в воздухе',
         choices=[
@@ -72,7 +85,10 @@ class SafetyClassAir(StructBlock):
         LitSource,
         label="Источник литературы для значения", 
         required=True)
+    abbr = 'Класс опасности в атмосферном воздухе'
+    long_name = 'Класс опасности в атмосферном воздухе' 
 
     class Meta:
         icon = 'image'
         label = 'Класс опасности в воздухе'
+        value_class = ClassStructValue
--- streams/structvalues.py
+++ streams/structvalues.py
@@ -16,12 +16,23 @@ class APIRepresentationMixin:
 
         api_json = {}
         if value:
-            api_json['B'] = value.Bj
+            if 'Bj' in dir(value):
+                api_json['B'] = value.Bj
             api_json['value'] = value['value']
             api_json['abbr'] = self.abbr
             api_json['name'] = self.long_name
             api_json['source'] = LitSourceSerializer(value['source']).data
             return api_json
+
+def get_b(value, low, medium, high):
+
+    if value < low:
+        return 1.
+    if value <= medium:
+        return 2.
+    if value <= high:
+        return 3.
+    return 4.
 
 
 class PDKsoilStructValue(StructValue):
@@ -29,10 +40,12 @@ class PDKsoilStructValue(StructValue):
     @property
     def Bj(self):
         value = self.get('value')
-        if value < 1:
-            return 1.
-        if value <= 10:
-            return 2.
-        if value <= 100:
-            return 3.
-        return 4.
+        return get_b(value, 1, 10, 100)
+
+
+class PDKairStructValue(StructValue):
+    
+    @property
+    def Bj(self):
+        value = self.get('value')
+        return get_b(value, 0.01, 0.1, 1)
