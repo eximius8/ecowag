6f3643963cd293b70d828746eee473225624a534
Mikhail Trunov <mikhail.trunov@gmail.com>
2022-06-13 03:52:32 +0300

    SSSL 2

--- ecowag/settings/production.py
+++ ecowag/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://webecolog.ru",
@@ -11,10 +11,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
-ALLOWED_HOSTS = ['api.webecolog.ru'] 
+ALLOWED_HOSTS = ['.api.webecolog.ru'] 
+CSRF_TRUSTED_ORIGINS = ['https://*.webecolog.ru'] 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+#SECURE_SSL_REDIRECT = True
 SECURE_HSTS_SECONDS = 60
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
