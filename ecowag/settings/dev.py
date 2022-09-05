adf3c9f0337f13157e7f76b21ea0c9a02956c40f
Mikhail Trunov <mikhail.trunov@gmail.com>
2022-09-06 01:11:49 +0300

    Update css

--- ecowag/settings/dev.py
+++ ecowag/settings/dev.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from .base import *
 
+# pip install python-dotenv
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
